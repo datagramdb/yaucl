58bf15935a769a8670a802a28ea0f9fe541fbb18
Giacomo Bergami <bergamigiacomo@gmail.com>
2021-07-16 08:59:37 +0200

    Adding pair serializer

--- include/yaucl/functional/iterators.h
+++ include/yaucl/functional/iterators.h
@@ -1,6 +1,22 @@
-//
-// Created by giacomo on 16/07/21.
-//
+/*
+ * iterators.h
+ * This file is part of yaucl
+ *
+ * Copyright (C) 2018 - Giacomo Bergami
+ *
+ * fuzzyStringMatching is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * fuzzyStringMatching is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with fuzzyStringMatching. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef YAUCL_ITERATORS_H
 #define YAUCL_ITERATORS_H
---
+++ include/yaucl/strings/serializer.h
@@ -0,0 +1,29 @@
+/*
+ * serializer.h
+ * This file is part of yaucl
+ *
+ * Copyright (C) 2018 - Giacomo Bergami
+ *
+ * fuzzyStringMatching is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * fuzzyStringMatching is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with fuzzyStringMatching. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef YAUCL_SERIALIZER_H
+#define YAUCL_SERIALIZER_H
+
+template <typename K, typename V>
+std::ostream& operator<< (std::ostream& out, const std::pair<K, V>& v) {
+    return out << "«" << v.first << ", " << v.second << "»";
+}
+
+#endif //YAUCL_SERIALIZER_H
