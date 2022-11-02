1f038df9398f42a6d7f7c92e7ccaaf49fdef905b
Giacomo Bergami <bergamigiacomo@gmail.com>
2022-11-02 10:37:20 +0000

    Changes I forgot (shouldn't I have already committed that remotely?). Plus, now it also compiles in Linux

--- src/knobab/server/algorithms/querymanager/MAXSatPipeline.cpp
+++ src/knobab/server/algorithms/querymanager/MAXSatPipeline.cpp
@@ -2,7 +2,7 @@
 // Created by giacomo on 16/04/2022.
 //
 
-
+#include <ios>
 #include <magic_enum.hpp>
 #include <knobab/server/operators/simple_ltlf_operators.h>
 #include <yaucl/functional/assert.h>
