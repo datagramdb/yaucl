8255e225818a4b5a68e5a782a09a51e55bc922a3
Giacomo Bergami <bergamigiacomo@gmail.com>
2022-11-29 23:14:48 +0000

    Ok

--- cmake-build-debug/CMakeFiles/yaucl_hashing.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for yaucl_hashing.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/submodule/roaring/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/giacomo/projects/yaucl-graph/submodule/roaring
-# Build directory: /home/giacomo/projects/yaucl-graph/cmake-build-debug/submodule/roaring
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
