b074d9999994b541cf2017d7907448c3ab451bd8
Giacomo Bergami <bergamigiacomo@gmail.com>
2023-01-16 22:07:29 +0000

    Yaucl updates

--- include/yaucl/strings/string_utils.h
+++ include/yaucl/strings/string_utils.h
@@ -16,6 +16,12 @@ namespace yaucl {
  */
         std::string utf8_tolower(const std::string &src);
         wchar_t  lowcase(wchar_t ch);
+
+        static inline bool is_number(const std::string& s)
+        {
+            return !s.empty() && std::find_if(s.begin(),
+                                              s.end(), [](unsigned char c) { return !std::isdigit(c); }) == s.end();
+        }
     }
 }
 
