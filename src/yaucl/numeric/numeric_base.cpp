c2bd0442fce4c5724abeeb36fa1d85d7a55ef09c
Giacomo Bergami <bergamigiacomo@gmail.com>
2023-01-19 15:00:08 +0000

    Fixing time bug

--- src/yaucl/numeric/numeric_base.cpp
+++ src/yaucl/numeric/numeric_base.cpp
@@ -15,17 +15,31 @@ std::vector <size_t> yaucl::numeric::numberToBase(size_t n, size_t b) {
 }
 
 #include <date/date.h>
+#include <chrono>
+#include <iomanip>
 
 long long int yaucl::numeric::parse8601(const std::string &save) {
     std::istringstream in{save};
     date::sys_time<std::chrono::milliseconds> tp;
     in >> date::parse("%FT%TZ", tp);
     if (in.fail())
-    {
+    try {
         in.clear();
         in.exceptions(std::ios::failbit);
         in.str(save);
         in >> date::parse("%FT%T%Ez", tp);
+    } catch(...) {
+        try {
+            std::tm tm = {};
+            std::stringstream ss( save );
+            ss >> std::get_time(&tm, "%Y-%m-%dT%H:%M:%OS6");
+            std::chrono::system_clock::time_point tp = std::chrono::system_clock::from_time_t(mktime(&tm));
+            return std::chrono::duration_cast<std::chrono::milliseconds>(tp.time_since_epoch()).count();
+        } catch(...) {
+            return std::chrono::duration_cast< std::chrono::milliseconds >(
+                    std::chrono::system_clock::now().time_since_epoch()
+            ).count();
+        }
     }
     return std::chrono::duration_cast<std::chrono::milliseconds>(tp.time_since_epoch()).count();
 }
