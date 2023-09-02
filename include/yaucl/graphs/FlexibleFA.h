a8398a05de5f5c39fcc4fd540d29f8183720d035
Giacomo Bergami <bergamigiacomo@gmail.com>
2023-09-02 15:08:18 +0100

    Required commit

--- include/yaucl/graphs/FlexibleFA.h
+++ include/yaucl/graphs/FlexibleFA.h
@@ -26,6 +26,7 @@
 #ifndef CLASSIFIERS_FLEXIBLEFA_H
 #define CLASSIFIERS_FLEXIBLEFA_H
 
+#include <random>
 #include <unordered_set>
 #include <yaucl/hashing/pair_hash.h>
 #include <yaucl/hashing/uset_hash.h>
@@ -42,7 +43,70 @@
 #include <roaring64map.hh>
 
 
+#include <queue>
 
+// C++ program for the above approach
+
+#include <bits/stdc++.h>
+
+template <typename T>
+struct QNode2 {
+    T data;
+    QNode2<T>* next;
+    QNode2(const T& d)
+    {
+        data = d;
+        next = NULL;
+    }
+};
+template <typename T>
+struct Queue2 {
+    QNode2<T> *front, *rear;
+    Queue2() { front = rear = NULL; }
+
+    void enQueue(T x)
+    {
+
+        // Create a new LL node
+        QNode2<T>* temp = new QNode2(x);
+
+        // If queue is empty, then
+        // new node is front and rear both
+        if (rear == NULL) {
+            front = rear = temp;
+            return;
+        }
+
+        // Add the new node at
+        // the end of queue and change rear
+        rear->next = temp;
+        rear = temp;
+    }
+
+    bool empty() const { return front == NULL; }
+    const T& top() const { return front->data; }
+
+    // Function to remove
+    // a key from given queue q
+    void pop()
+    {
+        // If queue is empty, return NULL.
+        if (front == NULL)
+            return;
+
+        // Store previous front and
+        // move front one node ahead
+        QNode2<T>* temp = front;
+        front = front->next;
+
+        // If front becomes NULL, then
+        // change rear also as NULL
+        if (front == NULL)
+            rear = NULL;
+
+        delete (temp);
+    }
+};
 
 template <typename NodeElement, typename EdgeLabel>
 class FlexibleFA : public FlexibleGraph<NodeElement, EdgeLabel> {
@@ -58,6 +122,53 @@ public:
         final_nodes.clear();
         removed_nodes.clear();
         removed_edges.clear();
+    }
+
+    //    std::set<std::vector<EdgeLabel>>
+    void generative(std::ofstream& tab,
+                    size_t min_len = 3,
+                    size_t max_len = 3,
+                    size_t max_per_limit = std::numeric_limits<size_t>::max(),
+                    bool doSample = false,
+                    double sampleRate = 0.5) {
+        if (max_per_limit == 0) return;
+        std::default_random_engine generator;
+        std::uniform_real_distribution<double> distribution(0.0,1.0);
+//        std::vector<size_t> Arg(getNodeIds().size(), 0);
+        Queue2<std::pair<size_t, std::vector<EdgeLabel>>> Q;
+        std::pair<size_t, std::vector<EdgeLabel>> cp;
+//        std::set<std::vector<EdgeLabel>> EL;
+        for (size_t i : init())
+            Q.enQueue(std::make_pair(i, std::vector<EdgeLabel>{}));
+        while (!Q.empty()) {
+            std::swap(cp, Q.front->data);
+            Q.pop();
+            if ((cp.second.size() <= max_len)) {
+                if (final_nodes.contains(cp.first) && (cp.second.size() > 0) /*&& (Arg[cp.first] < max_per_limit)*/) {
+//                    Arg[cp.first]++;
+//                    EL.emplace(cp.second);
+                    if (min_len <= cp.second.size()) {
+                        for (size_t j = 0, N = cp.second.size()-1; j<=N; j++) {
+                            tab << cp.second[j];
+                            if (j != N) tab  << "\t";
+                        }
+                        tab << std::endl;
+                        std::flush(tab);
+                        max_per_limit--;
+                        if (max_per_limit == 0) return;
+                    }
+                }
+                for (auto cp2 : outgoingEdges(cp.first)) {
+                    double number = distribution(generator);
+                    if ((!doSample) || number <= sampleRate) {
+                        std::vector<EdgeLabel> el{cp.second};
+                        el.emplace_back(cp2.first);
+                        Q.enQueue(std::make_pair(cp2.second, el));
+                    }
+                }
+            }
+        }
+//        return EL;
     }
 
     void clear() override {
